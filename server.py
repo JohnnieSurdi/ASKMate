d39cf4d2b52d66a418066f998869c6e2fd251260
Kamil Kornek <kamil.kornek96@gmail.com>
2022-01-31 10:47:50 +0100

    placeholder added

--- server.py
+++ server.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/list")
+def hello():
+    return "Hello World!"
+
+@app.route("/question/<question_id>")
+def hello():
+    return "Hello World!"
+
+@app.route("/add-question")
+def hello():
+    return "Hello World!"
+
+@app.route("/question/<question_id>/new-answer")
+def hello():
+    return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run()
