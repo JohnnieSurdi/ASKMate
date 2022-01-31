7944d8302630a398c9dd53063c1c1ba2d88a782e
Michal Prosniak <michalprosniak@gmail.com>
2022-01-31 13:10:57 +0100

    Creating new id for file

--- connection.py
+++ connection.py
@@ -13,3 +13,29 @@ def write_new_answer_to_file(new_answer):
         answers.write(file_answer_message)
 
 
+def rows_from_file(file):
+    csv_file_rows = []
+    with open(file, 'r') as csvfile:
+        for index, row in enumerate(csvfile):
+            if index != 0:
+                csv_file_rows.append(row)
+    return csv_file_rows
+
+
+def create_new_id(file):
+    max_id = 0
+    csv_file_rows = rows_from_file(file)
+    for row in csv_file_rows:
+        id_number = ""
+        for element in row:
+            if element != ",":
+                id_number += element
+            else:
+                if int(id_number) > max_id:
+                    max_id = int(id_number)
+                break
+    new_id = max_id + 1
+    return new_id
+
+
+# create_new_id('sample_data/answer.csv')
