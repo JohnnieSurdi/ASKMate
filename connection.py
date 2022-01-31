b0df09233e4e7e80694663cd8a4e5afc1cb957c7
Michal Prosniak <michalprosniak@gmail.com>
2022-01-31 14:14:40 +0100

    Multiple enters in message doesnt work, question id doesnt work, quotes in message doesnt work

--- connection.py
+++ connection.py
@@ -1,6 +1,8 @@
 def create_new_answer_for_file(new_answer):
     new_answer_list = []
-    for value in new_answer.values():
+    for index, value in enumerate(new_answer.values()):
+        if index == 4:
+            value = "\"" + value + "\""
         new_answer_list.append(str(value))
     file_answer_message = ", ".join(new_answer_list)
     file_answer_message += "\n"
@@ -9,9 +11,13 @@ def create_new_answer_for_file(new_answer):
 
 def write_new_answer_to_file(new_answer):
     file_answer_message = create_new_answer_for_file(new_answer)
+    print(file_answer_message)
     with open('sample_data/answer.csv', 'a+') as answers:
         answers.write(file_answer_message)
 
+
+# new_answer = {'id': 6, 'w}evw': 3, 'erqnbeqr': 2, 'qrbqrb': 0, 'message': 'kjbrqeirub', 'fqb': ''}
+# write_new_answer_to_file(new_answer)
 
 def rows_from_file(file):
     csv_file_rows = []
@@ -26,13 +32,13 @@ def create_new_id(file):
     max_id = 0
     csv_file_rows = rows_from_file(file)
     for row in csv_file_rows:
-        id_number = ""
+        id_number = 0
         for element in row:
-            if element != ",":
-                id_number += element
+            if element.isdigit():
+                id_number += int(element)
             else:
-                if int(id_number) > max_id:
-                    max_id = int(id_number)
+                if id_number > max_id:
+                    max_id = id_number
                 break
     new_id = max_id + 1
     return new_id
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import time
 import data_manager
-
+import connection
 from datetime import datetime
+
 
 app = Flask(__name__)
 adding_answer = {}
@@ -20,6 +21,8 @@ def list_questions():
 def display_question():
     return "Hello World!"
 
+
+
 @app.route("/add-question", methods=['GET','POST'])
 def add_question():
     if request.method == 'GET':
@@ -36,12 +39,13 @@ def add_question():
 @app.route("/question/<question_id>/new-answer", methods=['GET', 'POST'])
 def add_answer(question_id):
     if request.method == 'POST':
-        adding_answer['id'] = 1
-        adding_answer['submission_time'] = 1
+        adding_answer['id'] = connection.create_new_id('sample_data/answer.csv')
+        adding_answer['submission_time'] = time.time()
         adding_answer['vote_number'] = 0
         adding_answer['question_id'] = question_id
         adding_answer['message'] = request.form['new_answer']
         adding_answer['image'] = ""
+        connection.write_new_answer_to_file(adding_answer)
         return redirect('/question/<question_id>')
     return render_template('add_new_answer.html')
 
