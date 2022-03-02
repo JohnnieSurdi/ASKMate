662941d8bfe5831c24449906a0292a80e63e5c0c
PatrykCzaniecki <patrykczaniecki@gmail.com>
2022-03-02 11:24:08 +0100

    accept answer def

--- server.py
+++ server.py
@@ -351,5 +351,11 @@ def logout():
     return redirect('/')
 
 
+# accepting answers
+@app.route('//<question_id>/<answer_id>/accept_answer')
+def accept_answer():
+    pass
+
+
 if __name__ == "__main__":
     app.run()
