e69632b36fc084bb24437320609b75a1ee3279ba
Kamil Kornek <kamil.kornek96@gmail.com>
2022-03-02 10:47:44 +0100

    navbar added to all templates

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import data_manager
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 UPLOAD_FOLDER = 'static/uploads/'
 app = Flask(__name__)
-app.secret_key = "secret key"
+app.secret_key = "grgrewhre"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
